ef4384c4d8c0465b3d34ba3dd7c533cfc34b43ed
alexlecco <alexlecco24@gmail.com>
2018-03-06 15:07:05 -0300

    se creo el proyecto y se agrego el boton reset

---
+++ Android Sliding Puzzle/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Android_Sliding_Puzzle
+{
+    [Activity(Label = "Android_Sliding_Puzzle", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
