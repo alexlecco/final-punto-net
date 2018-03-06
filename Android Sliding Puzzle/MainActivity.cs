2c6554730148515312bc35ec81429e4966a23450
alexlecco <alexlecco24@gmail.com>
2018-03-06 16:17:24 -0300

    se creo la UI completa

--- Android Sliding Puzzle/MainActivity.cs
+++ Android Sliding Puzzle/MainActivity.cs
@@ -1,18 +1,47 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Graphics;
+using System;
 
 namespace Android_Sliding_Puzzle
 {
     [Activity(Label = "Android_Sliding_Puzzle", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        
+        Button resetButton;
+        GridLayout mainLayout;
+
+        int gameViewWidth;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            setGameView();
+        }
+
+        private void setGameView()
+        { 
+            resetButton = FindViewById<Button>(Resource.Id.resetButtonId);
+            resetButton.Click += resetMethod;
+
+            mainLayout = FindViewById<GridLayout>(Resource.Id.gameGridLayoutId);
+            gameViewWidth = Resources.DisplayMetrics.WidthPixels;
+
+            mainLayout.ColumnCount = 4;
+            mainLayout.RowCount = 4;
+
+            mainLayout.LayoutParameters = new LinearLayout.LayoutParams(gameViewWidth, gameViewWidth);
+            mainLayout.SetBackgroundColor(Color.Gray);
+
+        }
+        
+        private void resetMethod(object sender, EventArgs e)
+        {
+            
         }
     }
 }
